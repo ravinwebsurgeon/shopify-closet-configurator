135130399161e1f2197676400d2d7cb9ba35764b
ravinwebsurgeon <ravinwebsurgeon@gmail.com>
2025-03-28 19:23:18 +0530

    changes

--- src/components/ModalChildComponents/AddSectionComponent/AddSection.jsx
+++ src/components/ModalChildComponents/AddSectionComponent/AddSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./addSection.css";
+import "./AddSection.css";
 import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
