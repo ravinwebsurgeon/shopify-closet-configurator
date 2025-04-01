007c70ab506894d7034f7a824ab7a575bc9d0f47
ravinwebsurgeon <ravinwebsurgeon@gmail.com>
2025-04-01 17:24:36 +0530

    Shelves

--- src/components/ConfigurationTab/ConfigurationTab.jsx
+++ src/components/ConfigurationTab/ConfigurationTab.jsx
@@ -1,32 +1,23 @@
-import React, { useState } from "react";
-import "./Configurationtab.css";
-import DimensionsComponent from "../ConfigurationTabSubComponents/DimensionsComponent/DimensionsComponent";
-import ExecutionComponent from "../ConfigurationTabSubComponents/ExecutionComponent/ExecutionComponent";
-import ShelvesComponent from "../ConfigurationTabSubComponents/ShelvesComponent/ShelvesComponent";
-import { useDispatch, useSelector } from "react-redux";
-import { setActiveTab } from "../../slices/shelfDetailSlice";
+import React, { useState } from 'react'
+import'./Configurationtab.css'
+import DimensionsComponent from '../ConfigurationTabSubComponents/DimensionsComponent/DimensionsComponent';
+import ExecutionComponent from '../ConfigurationTabSubComponents/ExecutionComponent/ExecutionComponent';
+import ShelvesComponent from '../ConfigurationTabSubComponents/ShelvesComponent/ShelvesComponent';
 const ConfigurationTab = () => {
-  const activeTab = useSelector((state) => state.shelfDetail.racks.activeTab);  
-  const dispatch = useDispatch();
-  const handleSectionClick = (sectionKey) => {
-    dispatch(setActiveTab(sectionKey));
-  };
-  const configTabs = [
-    {
-      id: "dimensions",
-      label: "Dimensions",
-      component: <DimensionsComponent />,
-    },
-    { id: "execution", label: "Execution", component: <ExecutionComponent /> },
-    { id: "shelves", label: "Shelves", component: <ShelvesComponent /> },
-    { id: "sides", label: "Sides" },
-    { id: "backwalls", label: "Back walls" },
-    { id: "compartments", label: "Compartments" },
-    { id: "revolvingdoors", label: "Revolving doors" },
-    { id: "slidingdoors", label: "Sliding doors" },
-    { id: "drawers", label: "Drawers" },
-    { id: "wardroberods", label: "Wardrobe rods" },
-  ];
+    const [activeTab,setActiveTab] = useState('dimensions')
+    
+    const configTabs = [
+        { id: 'dimensions', label: 'Dimensions',component:<DimensionsComponent/> },
+        { id: 'execution', label: 'Execution',component:<ExecutionComponent/> },
+        { id: 'shelves', label: 'Shelves',component:<ShelvesComponent/> },
+        { id: 'sides', label: 'Sides' },
+        { id: 'backwalls', label: 'Back walls' },
+        { id: 'compartments', label: 'Compartments' },
+        { id: 'revolvingdoors', label: 'Revolving doors' },
+        { id: 'slidingdoors', label: 'Sliding doors' },
+        { id: 'drawers', label: 'Drawers' },
+        { id: 'wardroberods', label: 'Wardrobe rods' },
+      ];
 
   return (
     <div className="configuration-options">
@@ -34,8 +25,8 @@ const ConfigurationTab = () => {
         {configTabs.map((tab) => (
           <button
             key={tab.id}
-            className={`config-tab ${activeTab === tab.id ? "active" : ""}`}
-            onClick={() => handleSectionClick(tab.id)}
+            className={`config-tab ${activeTab === tab.id ? 'active' : ''}`}
+            onClick={() => setActiveTab(tab.id)}
           >
             {tab.label}
           </button>
@@ -43,12 +34,10 @@ const ConfigurationTab = () => {
       </div>
       <div className="config-content">
         {/* {renderTabContent()} */}
-        {configTabs.find((tab) => tab.id === activeTab)?.component || (
-          <p style={{ fontFamily: "Ubuntu" }}>Not yet available !!!</p>
-        )}
+        {configTabs.find((tab) => tab.id === activeTab)?.component || <p style={{fontFamily:"Ubuntu"}}>Not yet available !!!</p>}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ConfigurationTab;
+export default ConfigurationTab
